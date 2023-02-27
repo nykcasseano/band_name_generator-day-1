2603b1c09e08515e3f64fb3e3d18c1ab838819d0
nyk <nykcasseano13@gmail.com>
2023-02-27 10:27:50 -0300

    Versão final do código que corresponde ao exercício

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Greeting for you program.
+print("Seja bem vindo ao gerador de nomes para banda")
+
+#2. Ask the User for the city
+cidade = input("Qual é a Cidade que você cresceu?\n")
+
+#3 . Ask the user for the name of a pet.
+animal = input("Escreva um nome de Animal\n")
+
+#4. Combine the name of their city and pet and show them their band name
+print("O nome da sua banda poderia seria " + animal + " de " + cidade )
